f2b07b5af5ba4c7481d83b23bf3f9e7d497f59dc
Paulo Azevedo <paullo.azevedo@gmail.com>
2020-08-17 09:54:43 -0300

    Criação do projeto com dependencias primarias
    Criação do projeto com dependencias primarias

---
+++ template/src/test/java/br/com/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
